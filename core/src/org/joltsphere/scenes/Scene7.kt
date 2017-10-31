26a5a4c0471529880c4044abf14a8193ff38f647
Yusspy <fire6332@gmail.com>
2017-10-31 01:10:20 -0400

    Setup states and reward
    Next to make a Q matrix that finds the optimal actions to take on each state to maximize a reward.

--- core/src/org/joltsphere/scenes/Scene7.kt
+++ core/src/org/joltsphere/scenes/Scene7.kt
@@ -1,3 +1,11 @@
+/* 10/30/2017 - Yousef Abdelgaber
+*
+*   So I made this stick balancing game around two weeks ago, I had quite a bit of fun playing it
+*   but the purpose of it wasn't to entertain, it was to make an environment that I could learn to
+*   implement a Q-learning model in.
+*
+ */
+
 package org.joltsphere.scenes
 
 import org.joltsphere.main.JoltSphereMain
@@ -22,9 +30,21 @@ class Scene7(internal val game: JoltSphereMain) : Screen {
     internal var stick: Body
     internal var joint: RevoluteJoint
 
+    var isAutonomousEnabled = false
+    val learnRate = 0.5f
+    val discountFac = 1f
+
+    // STATES (2^3 TOTAL)
+    var horizontalStickSide = 0 // left is -1, right is 1
+    var verticalStickSide = 0 // below is -1, above is 1
+    var angularRotation = 0 // counterclockwise is -1, clockwise is 1
+    // THE THREE POSSIBLE ACTIONS AT EACH TIMESTEP IS MOVE-LEFT, MOVE-RIGHT, OR DO-NOTHING
+    var currentReward = 0f
+    var actualAngle = 0f
+
     init {
 
-        world = World(Vector2(0f, -9.8f), false) //ignore inactive objects false
+        world = World(Vector2(0f, -9.8f), false) // ignore inactive objects false
 
         debugRender = Box2DDebugRenderer()
 
@@ -53,24 +73,47 @@ class Scene7(internal val game: JoltSphereMain) : Screen {
         rdef.localAnchorB.set(0f, -700f/ppm)
         joint = world.createJoint(rdef) as RevoluteJoint
 
+        stick.applyLinearImpulse(Vector2(-10f, 0f), stick.localCenter, true)
+
     }
 
     internal fun update(dt: Float) {
 
-        if (Gdx.input.isKeyPressed(Keys.LEFT)) moveLeft(dt)
-        else if (Gdx.input.isKeyPressed(Keys.RIGHT)) moveRight(dt)
-        else if (base.linearVelocity.x < -0.1f) moveRight(dt)
-        else if (base.linearVelocity.x > 0.1f) moveLeft(dt)
-        else base.setLinearVelocity(0f,base.linearVelocity.y)
+        if (!isAutonomousEnabled && Gdx.input.isKeyJustPressed(Keys.Q)) isAutonomousEnabled = true
 
-        /*if (Gdx.input.isKeyPressed(Keys.UP)) moveUp(dt)
-        else if (Gdx.input.isKeyPressed(Keys.DOWN)) moveDown(dt)
-        else if (base.linearVelocity.y < -0.1f) moveUp(dt)
-        else if (base.linearVelocity.y > 0.1f) moveDown(dt)
-        else base.setLinearVelocity(base.linearVelocity.x, 0f)*/
+        if (isAutonomousEnabled) {
+            moveOptimally(dt)
+        }
+        else {
+            if (Gdx.input.isKeyPressed(Keys.LEFT)) moveLeft(dt)
+            else if (Gdx.input.isKeyPressed(Keys.RIGHT)) moveRight(dt)
+            else if (base.linearVelocity.x < -0.1f) moveRight(dt)
+            else if (base.linearVelocity.x > 0.1f) moveLeft(dt)
+            else base.setLinearVelocity(0f, base.linearVelocity.y)
+        }
+
+        updateRewardAndState();
 
     }
 
+    fun updateRewardAndState() {
+        // ANGLE = 0 is the upright postion of the stick and it increases to 360 degrees all the way around counterclockwise
+        actualAngle = (joint.jointAngle*180f/Math.PI).toFloat() // convert radians to degrees
+        while (actualAngle < 0) actualAngle += 360
+        while (actualAngle > 360) actualAngle -= 360
+        if (joint.jointSpeed < 0) angularRotation = 1
+        else if (joint.jointSpeed > 0) angularRotation = -1
+        if (actualAngle < 180) horizontalStickSide = -1
+        else if (actualAngle > 180) horizontalStickSide = 1
+        if (actualAngle < 90 && actualAngle > 270) verticalStickSide = 1
+        else verticalStickSide = -1
+        if (horizontalStickSide == -1) currentReward = (180f - actualAngle) / 180f
+        else if (horizontalStickSide == 1) currentReward = (actualAngle - 180f) / 180f
+    }
+
+    fun moveOptimally(dt: Float) {
+
+    }
     fun moveLeft(dt: Float) = base.setLinearVelocity(base.linearVelocity.x - 20*dt, 0f)
     fun moveRight(dt: Float) = base.setLinearVelocity(base.linearVelocity.x + 20*dt, 0f)
 
@@ -91,6 +134,8 @@ class Scene7(internal val game: JoltSphereMain) : Screen {
         game.batch.begin()
 
         game.font.draw(game.batch, "" + Gdx.graphics.framesPerSecond, game.width * 0.27f, game.height * 0.85f)
+        game.font.draw(game.batch, "R = " + Math.round(currentReward*1000f)/1000f, game.width * 0.1f, game.height * 0.1f)
+        game.font.draw(game.batch, "Angle = " + Math.round(actualAngle*10f)/10f, game.width * 0.8f, game.height * 0.1f)
 
         game.batch.end()
 
