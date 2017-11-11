129add709f88a4a93ed4edc0466c13db7bc5b24c
Yusspy <yabdelgaber@gmail.com>
2017-11-11 05:40:01 -0500

    Implemented a ton of the neural network
    So I had to go and learn linear regression and how a ReLu neural network works, but I think I got it. I just finished implementing all of the groundwork for the network, as well as the feedforward function, but I have yet to implement the backpropagation algorithim.

--- core/src/org/joltsphere/misc/NeuralNetwork.kt
+++ core/src/org/joltsphere/misc/NeuralNetwork.kt
@@ -1,17 +1,78 @@
 package org.joltsphere.misc
 
-class NeuralNetwork {
+/** Creates a fully fleshed out ReLu regression neural network
+ *  Feedforward on command, backpropagate to reduce error.
+ *
+ * @param numberOfInputs a value that sets the number of input neurons
+ * @param numberOfOutputs a value that sets the number of output neurons
+ * @param hiddenLayerConfiguration an int-array with the size of it setting the number of hidden layers, and with each corresponding value setting the number of neurons in that hidden layer
+ */
+class NeuralNetwork(val numberOfInputs: Int, val numberOfOutputs: Int, val hiddenLayerConfiguration: IntArray) {
 
-    fun setTrainingInputs(inputs: Array<FloatArray>) {
+    val numberOfHiddenLayers = hiddenLayerConfiguration.size
+    val networkSynapses: Array<Array<FloatArray>>
+
+    init {
+        val layerSizes = IntArray(2+numberOfHiddenLayers)
+        layerSizes[0] = numberOfInputs // first layer consists of the input neurons
+        layerSizes[layerSizes.size-1] = numberOfOutputs  // last layer is just the output neurons
+        for (i in 1 until layerSizes.size-1) layerSizes[i] = hiddenLayerConfiguration[i-1]+1 // for each of the hidden layers, set the layer size to the number of hidden layer neurons
+
+        networkSynapses = Array(layerSizes.size-1, // create a weight matrix for every layer of the neural network except the output
+                { i -> randomMatrix(layerSizes[i]+1,layerSizes[i+1]) }) // the matrix size is equal to (layer size + a bias weight) X (layer size of the next layer)
+    }
+
+    /** Feeds an array of inputs into the network to predict a set of outputs to the best of its ability in its current state.
+     * @param inputs an array of inputs equivalent to the number of input neurons
+     * @return an array of outputs corresponding to the output neurons with respect to the inputs
+     */
+    fun feedforward(inputs: FloatArray): FloatArray {
+        val input = Array(1, { inputs })
+        val output = Array(1, { FloatArray(numberOfOutputs) })
+        var justALayer = input // this is just a layer variable that is recycled throughout the feedforward process
+        for (i in 0 until networkSynapses.size) {
+            justALayer = matrixMultiply(addBiasColumn(justALayer), networkSynapses[i]) // get next layer by multiplying current layer with a bias column by the synapses for that layer
+            if (i != networkSynapses.size-1) justALayer = leakyReLu(justALayer) // if it isn't the last layer, which has no activation, then apply the the leaky ReLu activation function
+        }
+        return output[0] // there's only one array value anyway
+    }
+
+    /** Reduces the error of the neural network using gradient descent. Increasing the size of the training data mini-batches will increase the accuracy of the error reduction.
+     * @param trainingInputs a matrix of inputs with each row being a set of inputs for each input neuron
+     * @param targetOutputs a matrix with rows of the target output values corresponding to that row on the the training inputs
+     * Note that the training inputs matrix and target outputs matrix should have the same number of rows.
+     */
+    fun backpropagate(trainingInputs: Array<FloatArray>, targetOutputs: Array<FloatArray>, learningRate: Float) {
 
     }
 
-    fun feedforward() {
-
+    /** Adds a columns of 1's to your matrix! Have you ever wanted your layer's input values for the next layer to have a 1 in it to simulate a bias!?
+     *  Well now you can thanks to our revolutionary, simple, and instant "Add-A-Bias"© matrix technology!
+     */
+    fun addBiasColumn(matrix: Array<FloatArray>): Array<FloatArray> {
+        val output = Array(rows(matrix), { FloatArray(columns(matrix)+1) }) // creates a matrix of the equivalent size but with an extra columns
+        for (i in 0 until rows(matrix))
+            for (j in 0 until columns(matrix)) output[i][j] = matrix[i][j] // sets the values of the output to the values of the matrix
+        for (i in 0 until rows(output)) output[i][columns(output)-1] = 1f // set the bias values
+        return output
     }
 
-    fun backpropagate() {
-
+    // To prevent the death of neurons, a very small gradient is added in to the max function when x < 0
+    private val leakyReLuSlope = 0.01f
+    fun leakyReLu(x: Array<FloatArray>): Array<FloatArray> {
+        val output = x
+        for (i in 0 until rows(x))
+            for (j in 0 until columns(x))
+                if (x[i][j] < 0) output[i][j] *= leakyReLuSlope
+        return output
+    }
+    fun leakyReLuPrime(x: Array<FloatArray>): Array<FloatArray> {
+        val output = x
+        for (i in 0 until rows(x))
+            for (j in 0 until columns(x))
+                if (x[i][j] > 0) output[i][j] = 1f
+                else output[i][j] = leakyReLuSlope
+        return output
     }
 
 }
